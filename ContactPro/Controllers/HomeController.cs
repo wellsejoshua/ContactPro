0bbf2c241121c92d8e8e2810b7098053688a6435
Joshua Wells <codedbywells@gmail.com>
2022-09-17 11:16:43 -0500

    Register and Login page Clean up; Enabled 2 factor authentication

--- ContactPro/Controllers/HomeController.cs
+++ ContactPro/Controllers/HomeController.cs
@@ -4,29 +4,51 @@ using System.Diagnostics;
 
 namespace ContactPro.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [Route("/Home/HandleError/{code:int}")]
+    public IActionResult HandleError(int code)
+    {
+      var customError = new CustomError();
+
+      customError.code = code;
+
+      if (code == 404 )
+      {
+        customError.message = "The page you are looking for might have been removed had its name changed or is temporarily unavailable.";
+      }
+      else
+      {
+        customError.message = "Sorry, something went wrong";
+      }
+
+      return View("~/Views/Shared/CustomError.cshtml",customError);
+    }
+
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
