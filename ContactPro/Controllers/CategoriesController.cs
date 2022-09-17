0052a01edcb8fecd675a6805d5d22db903fd8eff
Joshua Wells <codedbywells@gmail.com>
2022-09-17 01:30:40 -0500

    Edited Categories Detail action to fix security flaw; Deleted Category Detail Action & View

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -93,25 +93,6 @@ namespace ContactPro.Controllers
       return View(ecvm);
     }
 
-    // GET: Categories/Details/5
-    [Authorize]
-    public async Task<IActionResult> Details(int? id)
-    {
-      if (id == null || _context.Categories == null)
-      {
-        return NotFound();
-      }
-
-      var category = await _context.Categories
-          .Include(c => c.AppUser)
-          .FirstOrDefaultAsync(m => m.Id == id);
-      if (category == null)
-      {
-        return NotFound();
-      }
-
-      return View(category);
-    }
 
     // GET: Categories/Create
     [Authorize]
@@ -207,10 +188,9 @@ namespace ContactPro.Controllers
       {
         return NotFound();
       }
-
+      string appUserId = _userManager.GetUserId(User);
       var category = await _context.Categories
-          .Include(c => c.AppUser)
-          .FirstOrDefaultAsync(m => m.Id == id);
+          .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
       if (category == null)
       {
         return NotFound();
@@ -224,17 +204,16 @@ namespace ContactPro.Controllers
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
-      if (_context.Categories == null)
-      {
-        return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
-      }
-      var category = await _context.Categories.FindAsync(id);
+      string appUserId = _userManager.GetUserId(User);
+      var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
       if (category != null)
       {
         _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
       }
 
-      await _context.SaveChangesAsync();
+
       return RedirectToAction(nameof(Index));
     }
 
