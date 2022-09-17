4c5ea6c490ef2e4a45d787caebc00fb3c574042c
Joshua Wells <codedbywells@gmail.com>
2022-09-17 12:15:13 -0500

    Added Connection string and programatic update-database for heroku

---
+++ ContactPro/Helpers/DataHelper.cs
@@ -0,0 +1,19 @@
+﻿using ContactPro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactPro.Helpers
+{
+    public static class DataHelper
+    {
+    //in Heroku this functions to equivalent to running update-database locally
+    //will only run from program.cs if there are changes to be made
+    public static async Task ManageDataAsync(IServiceProvider svcProvider)
+    {
+      //get an instance of the db application context
+      var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+      //migration: this is equivalent to update-database
+      await dbContextSvc.Database.MigrateAsync();
+    }
+
+    }
+}
--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -5,13 +5,18 @@ using Microsoft.EntityFrameworkCore;
 using ContactPro.Services;
 using ContactPro.Services.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using ContactPro.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 //var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 // Add services to the container.
+//two types of local connection strings
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -29,6 +34,9 @@ builder.Services.AddScoped<IEmailSender, EmailService>();
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+//get the database update with the latest migrations
+await DataHelper.ManageDataAsync(scope.ServiceProvider);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
